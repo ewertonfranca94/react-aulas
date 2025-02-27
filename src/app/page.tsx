386b39948baeada17dac627c4fe34745ff2d8569
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-02-27 02:59:38 -0300

    Resolução exercicio JSX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,16 @@ const Page = () => {
   }).format();
 
   const hour = new Date().getHours();
-  let horario = '';
 
-  if(hour >= 0 && hour <12){
-    horario = 'Bom Dia';
-  }else if(hour >= 12 && hour < 18){
-    horario = 'Boa Tarde';
-  }else{
-    horario = 'Boa Noite';
-  }
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white
     bg-gradient-to-r from-sky-500 to-indigo-500">
         <div className="text-9xl">{hora}</div>
-        <div className="text-5xl">{horario}</div>
+        <div className="text-5xl">
+          {hour >= 0 && hour < 12 && "Bom Dia"}
+          {hour >= 12 && hour <= 18 && "Boa Tarde"}
+          {hour >=18 && hour <=23 && "Boa Noite"}
+        </div>
     </div>  
       
   );
