32b7a6bea6528a6df7a961c970a245119adb40a0
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-02-28 00:12:12 -0300

    Resolução exercicio Emoji - versão simples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
+import { Emoji } from "@/componentes/Emoji";
+
 const Page = () => {
-  const hora = new Intl.DateTimeFormat('pt-BR',{
-    timeStyle: 'short',
-    hour12: false
-  }).format();
-
-  const hour = new Date().getHours();
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white
-    bg-gradient-to-r from-sky-500 to-indigo-500">
-        <div className="text-9xl">{hora}</div>
-        <div className="text-5xl">
-          {hour >= 0 && hour < 12 && "Bom Dia"}
-          {hour >= 12 && hour <= 18 && "Boa Tarde"}
-          {hour >=18 && hour <=23 && "Boa Noite"}
-        </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Emoji nota={5}/>
     </div>  
       
   );
