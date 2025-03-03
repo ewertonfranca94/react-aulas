4f7b79c282e6c6fa62e3f0a9d8841758189c1a85
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-03-03 01:08:48 -0300

    Layout do exercicio pronto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 import { EstudanteTable } from "@/componentes/EstudantesTable";
 import { estudante } from "@/data/estudantes";
 
-import { EstudanteTable } from "@/componentes/EstudantesTable";
-import { estudante } from "@/data/estudantes";
-
 const Page = () => {
   return (
     <div className="container mx-auto text-center">
       <h1 className="text-xl mm-5"> Lista de estudantes</h1>
       <EstudanteTable estudantes = {estudante}/>
-
-    <div className="container mx-auto text-center">
-      <h1 className="text-xl mm-5"> Lista de estudantes</h1>
-      <EstudanteTable estudantes = {estudante}/>
-
-    </div>  
+    </div>
       
   );
 }
