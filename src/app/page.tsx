7e1a4f0d7880fcae13d7ae9f02a519151208fe4c
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-03-06 03:49:15 -0300

    Passando eventos com parametros - Arrow Function - Modulo 3 aula 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import { EstudanteTable } from "@/componentes/EstudantesTable";
-import { estudante } from "@/data/estudantes";
+"use client"
 
 const Page = () => {
+
+  const avisar = (msg: string) => {
+    alert(msg);
+  }
+
+
   return (
-    <div className="container mx-auto text-center">
-      <h1 className="text-xl mm-5"> Lista de estudantes</h1>
-      <EstudanteTable estudantes = {estudante}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button onClick={() => avisar('Mensagem 1')}className="p-3  bg-blue-700 text-white mr-3">Clique aqui 1</button>
+      <button onClick={() => avisar('Mensagem 2')}className="p-3  bg-blue-700 text-white">Clique aqui 2</button>
     </div>
       
   );
