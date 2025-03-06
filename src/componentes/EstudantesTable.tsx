cde611b4982a6964c2d1bb85be7b26f4b3b10766
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-03-06 03:28:06 -0300

    resolução do exercicio aula 18

--- src/componentes/EstudantesTable.tsx
+++ src/componentes/EstudantesTable.tsx
@@ -12,10 +12,30 @@ export const EstudanteTable = ({estudantes} : Props) => {
                     <th className="p-3 text-white">Status</th>
                     <th className="p-3 text-white">Grade 1</th>
                     <th className="p-3 text-white">Grade 2</th>
+                    <th className="p-3 text-white">Final Grade</th>
                 
                 </tr>
             </thead>
             <tbody>
+                {estudantes.map(item =>(
+                    <tr key={item.id} className="text-gray-800 bg-gray-400 border-gray-600">
+                        <td className="p-3 flex items-center">
+                            <img src={item.avatar} alt={item.name} className="w-10 h-10 roudeded-full mr-3"/>
+                            <div>
+                                <div className="text-left font-bold">{item.name}</div>
+                                <div>{item.email}</div>
+                            </div>
+                        </td>
+                        <td>
+                            {item.active && <div className="px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-xs">Active</div>}
+                            {!item.active && <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-xs">Inactive</div>}
+                        </td>
+                        <td className="p-3">{item.grade1.toFixed(1)}</td>
+                        <td className="p-3">{item.grade2.toFixed(1)}</td>
+                        <td className="p-3 font-bold">{item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--' }</td>
+
+                    </tr>
+                ))}
 
             </tbody>
         </table>
