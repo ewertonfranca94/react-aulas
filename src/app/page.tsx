9b4857922f07804ffd5f8f68082c54aac43958ab
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-03-09 00:14:47 -0300

    Modulo 3 - Aula 8 - exemplo de states  em campos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,24 @@ import { useState } from "react";
 
 const Page = () => {
 
+  const [nameInput, setNameInput] = useState('');
+
+  const handleBtnClick = () => {
+    alert(nameInput);
+
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-
+      <input 
+        type="text"  
+        className="border border-black p-3 text-xl text-black"
+        placeholder="Digite seu nome"
+        value={nameInput}
+        onChange={event => setNameInput(event.target.value)}
+      />
+      <p>{nameInput}</p>
+      <button onClick={handleBtnClick}>Mostrar Valor </button>
     </div>
   );
 }
