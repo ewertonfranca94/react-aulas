f94367460fb912d91dde732f68716ce0a0952389
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-03-08 23:54:40 -0300

    Modulo 3 - Aula 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,11 @@
 import { useState } from "react";
 
 const Page = () => {
-  const  [showSecret, setShowSecret] = useState(false);
-  
-  const handleClickButton = () => {
-    setShowSecret(!showSecret);
-  }
+
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <button onClick={handleClickButton} className="bg-blue-500 p-3">{showSecret ? 'Ocultar' : 'Mostrar'}</button>
-      {showSecret &&
-      <div className="p-3 bg-blue-300 rounded-md mt-3">Area secreta</div>
-      }
+
     </div>
   );
 }
